e516483480b450aece200bb05789eebe03b0ef53
Emily Lillie <emilylillie3@gmail.com>
2019-07-19 13:21:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  #array.sort do |a, b|
+ # b <=> a
+#end
+array.sort.reverse
+end
+
+def sort_array_char_count(array)
+   array.sort do |left, right|
+     left.length <=> right.length
+   end
+ end
+ 
+ 
